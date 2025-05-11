62842b08c62e7989659174ffe35a3b2ce970729f
palak-glitch <palakpandey2912@gmail.com>
2025-05-11 19:53:08 +0530

    final Commit

--- lib/Utils/utils.dart
+++ lib/Utils/utils.dart
@@ -5,19 +5,4 @@ double convertTemperature(double? tempCelsius, bool isCelsius) {
   return isCelsius ? tempCelsius : (tempCelsius * 9 / 5) + 32;
 }
 
-String formatTemperature({
-  required double? tempCelsius,
-  required bool isCelsius,
-  required bool symbolFirst,
-  required String locale,
-}) {
-  if (tempCelsius == null) return '--';
-
-  final temp = convertTemperature(tempCelsius, isCelsius);
-  final unit = isCelsius ? '°C' : '°F';
-  final formatter = NumberFormat('#0.0', locale);
-  final formattedTemp = formatter.format(temp);
-
-  return symbolFirst ? '$unit $formattedTemp' : '$formattedTemp $unit';
-}
 
